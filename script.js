3e89449a34fb43c84625d274d68dc725fdd1147c
unknown <nirupama1210@gmail.com>
2021-08-05 21:49:24 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-wt=parseFloat(prompt("Enter Weight (in kg)"));
-ht=parseFloat(prompt("Enter Height (in m)"));
+function calc1(){
+wt=parseFloat(document.getElementById("wght").value);
+ht=parseFloat(document.getElementById("hgt").value);
+wgt=document.getElementById("s1").value;
+hgt=document.getElementById("s2").value;
+if(wgt==="lbs")
+{
+    wt=wt*0.453//Converting lbs to kg
+}
+if(hgt==="in")
+{
+    ht=ht*0.0254//Converting in to m
+}
 bmi=wt/(ht**2);
+s="";
 if(bmi<18.5)
 {
-    document.write("Underweight and BMI is "+bmi.toFixed(2));
+    s=("Underweight and BMI is "+bmi.toFixed(2));
 }
 else if(bmi<=24.9)
 {
-    document.write("Normal and BMI is "+bmi.toFixed(2));
+    s=("Normal and BMI is "+bmi.toFixed(2));
 }
 else if(bmi<=29.9)
 {
-    document.write("Overweight and BMI is "+bmi.toFixed(2));
+    s=("Overweight and BMI is "+bmi.toFixed(2));
 }
 else if(bmi <=34.9)
 {
-    document.write("Obese Class 1 and BMI is "+bmi.toFixed(2));
+    s=("Obese Class 1 and BMI is "+bmi.toFixed(2));
 }
 else if(bmi<=39.9)
 {
-    document.write("Obese Class II and BMI is "+bmi.toFixed(2));
+    s=("Obese Class II and BMI is "+bmi.toFixed(2));
 }
 else{
-    document.write("Obese Class III and BMI is "+bmi.toFixed(2));
+    s=("Obese Class III and BMI is "+bmi.toFixed(2));
+}
+document.getElementById("d2").innerHTML=s;
+}
+
+function reset(){
+    document.getElementById("d2").innerHTML="&nbsp;";
+    document.getElementById("wght").value="";
+    document.getElementById("hgt").value="";
 }
